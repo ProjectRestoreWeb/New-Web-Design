5b89d7f0a63548ac7c0e92aa0d1b08beca12fbe6
SuperByakugan <43621807+SuperByakugan@users.noreply.github.com>
2019-07-14 20:42:45 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,23 +10,28 @@ firebase.auth().onAuthStateChanged(function(user) {
 
 function login()
 {
+  document.getElementById("loginPopUp").style.display = "block";
+  document.getElementById("loginPopUp").innerHTML = "This might take a moment";
+
   var userEmail = document.getElementById("emailLogin").value;
   var userPassword = document.getElementById("passwordLogin").value;
 
-  console.log(userEmail + " " + userPassword);
-
   firebase.auth().signInWithEmailAndPassword(userEmail, userPassword).catch(function(error) {
+
   // Handle Errors here.
+
   var errorCode = error.code;
   var errorMessage = error.message;
-
-  window.alert(errorMessage);
+  document.getElementById("loginPopUp").innerHTML = errorMessage;
   // ...
   });
 }
 
 function logout()
 {
+  document.getElementById("loginPopUp").style.display = "none";
+  document.getElementById("signupPopUp").style.display = "none";
+
   firebase.auth().signOut().then(function() {
   // Sign-out successful.
   }).catch(function(error) {
@@ -36,13 +41,19 @@ function logout()
 
 function signUp()
 {
+  document.getElementById("signupPopUp").style.display = "block";
+  document.getElementById("signupPopUp").innerHTML = "This might take a moment";
+
   var userEmailS = document.getElementById("emailSignUp").value;
   var userPasswordS = document.getElementById("passwordSignUp").value;
 
   firebase.auth().createUserWithEmailAndPassword(userEmailS, userPasswordS).catch(function(error) {
   // Handle Errors here.
+
   var errorCode = error.code;
   var errorMessage = error.message;
+
+  document.getElementById("signupPopUp").innerHTML = errorMessage;
   // ...
   });
 }
